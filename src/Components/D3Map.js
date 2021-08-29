657fdef3eddf09efa2a3ca1a6011f2f8127d8e85
tapptm <phuwadet00123@gmail.com>
2021-08-29 22:57:14 +0700

    update map response ui

--- src/Components/D3Map.js
+++ src/Components/D3Map.js
@@ -6,6 +6,8 @@ import * as d3 from "d3";
 import axios from "axios";
 import "./d3.css";
 
+const url = "http://localhost:3000";
+
 const MapComponent = () => {
   const mapData = async () => {
     try {
@@ -18,21 +20,9 @@ const MapComponent = () => {
     }
   };
 
-  var tooltipEl = function (d) {
-    return (
-      '<div class="tip__container">' +
-      '<div class="val">' +
-      d.id +
-      "</div>" +
-      '<div class="close">' +
-      "<button>&times</button>" +
-      "</div>" +
-      "</div>"
-    );
-  };
   function D3Layer() {
     const map = useMap();
-    let radius = 15;
+    let radius = 19;
     useEffect(async () => {
       const svgLayer = L.svg({ clickable: true });
       svgLayer.addTo(map);
@@ -60,13 +50,78 @@ const MapComponent = () => {
       const g = svg.select("g").attr("class", "leaflet-zoom-hide");
       const defs = svg.append("svg:defs");
 
+      let tooltipEl = function (d) {
+        const project_url = url + "?project_id=" + d.project_id;
+        const co_url = url + "?co_id=" + d.coresearcher_id;
+        const user_url = url + "?user_idcard=" + d.user_idcard;
+
+        if (d.project_id)
+          return `
+        <div class="tip__container">  
+          <div class="val"><h6>${d.label}</h6></div>
+          <div class="close">
+            <button>&times</button>
+          </div>
+          <hr/>
+          <div class="val">${d.project_name}</div>
+          <div class="val">lat:${d.lat} </br> lng:${d.lon}</div>
+          <hr/>
+          <a href="${project_url}" class="btn">รายละเอียดเพิ่มเติม</a> 
+        </div>`;
+
+        if (d.coresearcher_id)
+          return `
+        <div class="tip__container">  
+          <div class="val">${d.id} ${d.label}</div>
+          <div class="close">
+            <button>&times</button>
+          </div>
+          <hr/>
+          <div class="val">${d.coresearcher_id}</div>
+          <hr/>
+          <a href="${co_url}" class="btn">รายละเอียดเพิ่มเติม</a>
+          
+        </div>`;
+
+        if (d.user_idcard)
+          return `
+        <div class="tip__container">  
+          <div class="val"><h6>${d.label}</h6></div>
+          <div class="close">
+            <button>&times</button>
+          </div>
+          <hr/>
+          <div class="val">${d.user_idcard}</div>
+          <hr/>
+          <a href="${user_url}" class="btn">รายละเอียดเพิ่มเติม</a>         
+        </div>`;
+
+        return `
+        <div class="tip__container">  
+          <div class="val">${d.id}</div>
+          <div class="close">
+            <button>&times</button>
+          </div>  
+          <hr/>
+          <div class="val">nodata</div>
+          <hr/>
+          
+      </div>`;
+      };
+      // Tool Tip
+      const div = d3
+        .select(map.getPanes().overlayPane)
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
       const links = g
         .selectAll("line")
         .data(data.links)
         .join("line")
-        .attr("stroke", "red")
-        .attr("stroke-opacity", 0.3)
-        .attr("stroke-width", 5);
+        .attr("stroke", "lime")
+        .attr("stroke-opacity", 0.8)
+        .attr("stroke-width", 3);
 
       const nodes = g
         .selectAll("circle")
@@ -74,53 +129,128 @@ const MapComponent = () => {
         .join("circle")
         .attr("id", (d) => `node-${d.id}`)
         .attr("r", radius)
-        .attr("stroke", "white");
-      // .attr("fill", "#555")
-      // .attr("fill", (d) => {
-      //   let imgSize = d.radius * 2;
-      //   defs
-      //     .append("svg:pattern")
-      //     .attr("id", `node-img-id${d.id}`)
-      //     .attr("width", imgSize)
-      //     .attr("height", imgSize)
-      //     .append("svg:image")
-      //     .attr("xlink:href", d.img)
-      //     .attr("width", imgSize)
-      //     .attr("height", imgSize)
-      //     .attr("x", 0)
-      //     .attr("y", 0);
-      //   return `url(#node-img-id${d.id})`;
-      // });
-
-      // nodes.on("mouseover", function () {
-      //   d3.select(this)
-      //     .transition()
-      //     .duration("150")
-      //     .attr("stroke", "blue")
-      //     .attr("r", radius * 2);
-      // });
-
-      // nodes.on("mouseout", function () {
-      //   d3.select(this)
-      //     .transition()
-      //     .duration("150")
-      //     // .attr("stroke", "red")
-      //     .attr("r", radius);
-      // });
-
-      nodes.on("click", function () {
-        d3.select(this).attr("stroke", (d) => {
-          console.log(d);
-          // here you can access data of node using d.key
-          alert("You clicked on node " + d.name);
+        .attr("stroke", "white")
+        .attr("stroke-width", 2.5)
+        .attr("fill", "#555")
+        .attr("fill", (d) => {
+          let imgSize = d.radius * 2;
+          defs
+            .append("svg:pattern")
+            .attr("id", `node-img-id${d.id}`)
+            .attr("width", imgSize)
+            .attr("height", imgSize)
+            .append("svg:image")
+            .attr("xlink:href", d.img)
+            .attr("width", imgSize)
+            .attr("height", imgSize)
+            .attr("x", 0)
+            .attr("y", 0);
+          return `url(#node-img-id${d.id})`;
         });
 
+      nodes.on("mouseover", function () {
+        d3.select(this)
+          .transition()
+          .duration("150")
+          // .attr("stroke", "blue")
+          .attr("r", radius * 1.5);
+      });
+
+      // nodes.on("mousemove", (event) => {
+      //   const [x, y] = d3.pointer(event);
+      //   div.style("top", y - 10 + "px").style("left", x + 10 + "px");
+      // });
+
+      nodes.on("mouseout", function () {
+        d3.select(this)
+          .transition()
+          .duration("150")
+          // .attr("stroke", "red")
+          .attr("r", radius);
+      });
+
+      const neighbours = {};
+
+      data.nodes.forEach((node) => {
+        neighbours[node.id] = neighbours[node.id] || [];
+      });
+      // console.log(neighbours);
+      data.links.forEach((link) => {
+        neighbours[link.source].push(link.source);
+        neighbours[link.source].push(link.target);
+        neighbours[link.target].push(link.target);
+        neighbours[link.target].push(link.source);
+      });
+
+      function deHighlight() {
+        nodes.style("opacity", 1);
+        links.style("opacity", 1);
+        label.style("visibility", "hidden");
         links.attr("stroke", (d) => {
-          return `node-${d.source.id}` == this.id ||
-            `node-${d.target.id}` == this.id
-            ? "blue"
-            : "red";
+          // console.log(d.source.id);
+          return `node-${d.source.id}` == d.id || `node-${d.target.id}` == d.id
+            ? "red"
+            : "lime";
         });
+      }
+
+      function highlightNeighbours(d) {
+        deHighlight();
+
+        nodes.style("opacity", (n) => {
+          return neighbours[d.id].indexOf(n.id) != -1 ? 1 : 0.1;
+        });
+
+        links.style("opacity", (n) => {
+          return d.id == n.source.id || d.id == n.target.id ? 1 : 0.1;
+        });
+
+        label
+          .filter((n) => neighbours[d.id].indexOf(n.id) != -1)
+          // we can't use display:none with labels because we need to load them in the DOM in order to calculate the background rectangle dimensions with the getBBox function.
+          // So we used visibility:hidden instead.
+          .style("visibility", "visible");
+      }
+
+      nodes
+        .on("click", function (event, d) {
+          const [x, y] = d3.pointer(event);
+
+          div.html(tooltipEl(d));
+
+          div.select("button").on("click", function () {
+            div.style("opacity", 0);
+            div.style("visibility", "hidden");
+            // deHighlight();
+          });
+
+          div
+            .transition()
+            .duration(200)
+            .style("display", "block")
+            .style("visibility", "visible")
+            .style("opacity", 1);
+
+          div.style("top", y + 30 + "px").style("left", x - 30 + "px");
+
+          // connectedNodes(d);
+          highlightNeighbours(d);
+
+          links.attr("stroke", (d) => {
+            // console.log(d.source.id);
+            return `node-${d.source.id}` == this.id ||
+              `node-${d.target.id}` == this.id
+              ? "red"
+              : "lime";
+          });
+        })
+        .on("dblclick", () => {
+          deHighlight();
+        });
+
+      map.on("click", function (e) {
+        div.style("opacity", 0);
+        div.style("visibility", "hidden");
       });
 
       const label = g
@@ -129,12 +259,9 @@ const MapComponent = () => {
         .enter()
         .append("text")
         .text(function (d) {
-          return `${d.id} ${d.type}`;
+          return `${d.label}`;
         })
-        .style("text-anchor", "middle")
-        .style("fill", "black")
-        .style("font-family", "Arial")
-        .style("font-size", 12);
+        .attr("class", "label");
 
       const drawAndUpdate = () => {
         links
@@ -220,7 +347,7 @@ const MapComponent = () => {
   return (
     <MapContainer
       center={center}
-      zoom={9}
+      zoom={10}
       // minZoom={6}
       // scrollWheelZoom={true}
       // zoomControl={false}
